15f9c211edfb10d32489d57482fd5a6a04250f3a
liamcr <liamcrocket@gmail.com>
2020-02-22 16:01:50 -0500

    Updated musidex to musicpedia

--- src/custom/featuredProjects.ts
+++ src/custom/featuredProjects.ts
@@ -4,15 +4,15 @@ import PartyMixPreview from "../assets/partyMixPreview.png";
 
 export const featuredProjects = [
   {
-    title: "Musidex",
+    title: "Musicpedia",
     date: "Winter 2020",
     technologies: ["reactjs", "materialUI"],
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     imageReference: Musidex,
     projectPageURL: "#",
-    url: "https://musidex.web.app/",
-    repoURL: "https://github.com/liamcr/musidex"
+    url: "https://musicpedia-app.web.app/",
+    repoURL: "https://github.com/liamcr/musicpedia"
   },
   {
     title: "One Word Stories",
