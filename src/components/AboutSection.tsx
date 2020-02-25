155d370abf6077ef1e69ba900165a2364c605aa0
liamcr <liamcrocket@gmail.com>
2020-02-24 19:14:46 -0500

    Added first draft of 'about me' section paragraph

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -10,20 +10,19 @@ const AboutSection: React.FC = () => {
       <div className="about-content-container">
         <img className="about-image" src={AboutImage} alt="Me" />
         <div className="about-content">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
-          dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-          velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-          occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-          mollit anim id est laborum.
+          Welcome to the site! I’m Liam Crocket, and I’m currently working
+          through my 3rd year in the University of Guelph’s Software Engineering
+          Program. I’m passionate about web development in particular, but am
+          also interested in learning about all sorts of other development
+          platforms, such as mobile or desktop development. On this site you can
+          see how I’ve put this passion into my projects by visiting and
+          checking them out below! I currently have 3 co-op terms worth of
+          experience under my belt (plus 1 internship!), with responsibilities
+          including systems development, full-stack web development, and machine
+          learning engineering. I’m in the process of searching for an 8-month
+          co-op position for the winter 2021 term, so if you’re interested feel
+          free to reach out using any of the links listed in the contact section
+          below!
         </div>
       </div>
     </div>
