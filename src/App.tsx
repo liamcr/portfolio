c3ed3bf5bc7ed199f437cbebf99f360c67573c66
liamcr <liamcrocket@gmail.com>
2020-02-25 20:08:32 -0500

    Configuring gh-pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
 import React from "react";
-import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import "./App.css";
 
 const App: React.FC = () => {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          <HomePage />
-        </Route>
-        <Route path="/page-not-found">
-          <div>Page not found!</div>
-        </Route>
-        <Redirect from="*" to="/page-not-found" />
-      </Switch>
-    </BrowserRouter>
-  );
+  return <HomePage />;
 };
 
 export default App;
