d644b47b6a849279119b74daba579b39c1e12913
lcrocket <liamcrocket@gmail.com>
2020-06-27 12:38:59 -0400

    Added ApplyBoard entry to work experience section

--- src/custom/workExperience.ts
+++ src/custom/workExperience.ts
@@ -1,8 +1,21 @@
+import ApplyBoardLogo from "../assets/companyLogos/applyboard.png";
 import WSIBLogo from "../assets/companyLogos/wsib.png";
 import CooperatorsLogo from "../assets/companyLogos/cooperators.png";
 import NumeratorLogo from "../assets/companyLogos/numerator.png";
 
 export const workExperience = [
+  {
+    jobTitle: "Full-Stack Developer (Co-op)",
+    company: "ApplyBoard",
+    location: "Kitchener, ON",
+    timeFrame: "May 2020 - Aug 2020",
+    companyImage: ApplyBoardLogo,
+    highlights: [
+      "Utilized my skills in Ruby on Rails to assist in developing the backend of the company's site, and leveraged my experience with test-driven development to ensure stable code quality",
+      "Implemented frontend features on a site viewed by over 100,000 users by applying my React and JavaScript skills",
+      "Developed microservice endpoints using a Python backend",
+    ],
+  },
   {
     jobTitle: "Full-Stack Developer (Co-op)",
     company: "WSIB Innovation Lab",
@@ -12,8 +25,8 @@ export const workExperience = [
     highlights: [
       "Utilized object-oriented programming in Swift, as well as machine learning concepts, to develop an iOS application to determine when a worker is fatigued on the job",
       "Assisted in developing the Lab’s website using React, as well as a project portfolio used by upper-level executives",
-      "Leveraged time-management and organizational skills to accelerate productivity on core research projects within a small team"
-    ]
+      "Leveraged time-management and organizational skills to accelerate productivity on core research projects within a small team",
+    ],
   },
   {
     jobTitle: "Jr. Systems Developer (Co-op)",
@@ -24,8 +37,8 @@ export const workExperience = [
     highlights: [
       "Performed technical analysis and formulated design and logic for programs",
       "Assisted in planning and executing unit tests to verify that functionality and data integrity is maintained",
-      "Provided status reports and escalated issues when necessary to lead resource"
-    ]
+      "Provided status reports and escalated issues when necessary to lead resource",
+    ],
   },
   {
     jobTitle: "Media Analyst Intern",
@@ -36,7 +49,7 @@ export const workExperience = [
     highlights: [
       "Curated a preliminary digital website tracking universe consisting of over 1,000 websites",
       "Analyzed over 1,500 advertisement records in a database to check for miscut advertisements and ensure data integrity",
-      "Cross-referenced advertising information to distinguish nationally run advertisements from locally run advertisements during a dispute between television broadcasting companies Univision and UniMas"
-    ]
-  }
+      "Cross-referenced advertising information to distinguish nationally run advertisements from locally run advertisements during a dispute between television broadcasting companies Univision and UniMas",
+    ],
+  },
 ];
